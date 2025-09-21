6c22f6b22f06f9d777745ec6b5534a64772ebadb
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-09-21 16:45:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion Sort/insertion-sort.java
@@ -0,0 +1,26 @@
+class Solution {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        // code here
+        for(int i=1;i<arr.length;i++){
+            for(int j=i;j>0;j--){
+                if(compare(arr,j-1,j)==true){
+                    swap(arr,j-1,j);
+                }else{
+                    break;
+                }
+            }
+        }
+        
+    }public boolean compare(int[]arr ,int l,int r){
+        if(arr[l]>arr[r]) return true;
+        else if(arr[l]<arr[r]) return false;
+        else return false;
+    }
+    public void swap(int []arr,int l,int r){
+        int temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        return;
+    }
+}
