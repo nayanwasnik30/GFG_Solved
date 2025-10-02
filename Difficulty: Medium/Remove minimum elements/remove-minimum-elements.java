8dbf907364e031f0294c96b2c051ead568e211f2
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-03 01:13:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Remove minimum elements/remove-minimum-elements.java
+++ Difficulty: Medium/Remove minimum elements/remove-minimum-elements.java
@@ -5,24 +5,22 @@ class Solution {
     public int minRemoval(int[] arr) {
         // code here
         Arrays.sort(arr);
-        int n=arr.length;
-        int min=Integer.MAX_VALUE;
+        int min=arr.length;
         for(int i=0;i<arr.length;i++){
-            int num=2*arr[i]+1;
-            int idx=floor(arr,num);
-            int trem=n-(idx-i+1);
-            min=Math.min(min,trem);
+            int floorIdx=floor(arr,(arr[i]*2)+1);
+            int temp=arr.length-(floorIdx-i+1);
+            
+            min=Math.min(temp,min);
         }
-        return min;
         
+        return min;
     }
-    public int floor(int [] arr,int target){
+    
+    public int floor(int []arr,int target){
         int left=0;
         int right=arr.length-1;
-        
         while(left<=right){
             int mid=left+(right-left)/2;
-            
             if(arr[mid]==target){
                 right=mid-1;
             }else if(arr[mid]<target){
@@ -31,6 +29,7 @@ class Solution {
                 right=mid-1;
             }
         }
+        
         return right;
     }
 }
