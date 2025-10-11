d054bbf949b862f4f1335d09479944a494eefd98
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-11 23:08:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
+++ Difficulty: Medium/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
@@ -2,20 +2,19 @@ class Solution {
     int countPairs(int[][] mat1, int[][] mat2, int target) {
         // code here
         int n=mat1.length;
-        int left=0;int right=(n*n)-1;
+        int left=0;
+        int right=(n*n)-1;
         int count=0;
         while(left<n*n && right>=0){
-            //co-ordinate
-            int row1=left/n;int col1=left%n;
-            int row2=right/n;int col2=right%n;
-            int sum=mat1[row1][col1]+mat2[row2][col2];
+            // 2d array to 1d array
+            int r1=left/n; int c1=left%n;
+            int r2=right/n; int c2=right%n;
+            int sum=mat1[r1][c1]+mat2[r2][c2];
             if(sum==target){
                 count++;
                 left++;
                 right--;
-                continue;
-            }
-            else if(sum<target){
+            }else if (sum<target){
                 left++;
             }else{
                 right--;
