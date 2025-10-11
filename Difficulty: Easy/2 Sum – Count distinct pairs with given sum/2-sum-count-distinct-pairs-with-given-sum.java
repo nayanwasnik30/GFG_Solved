dbf6540ee74bfdc1694ac45892534389f7d12297
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-10-11 21:33:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/2 Sum – Count distinct pairs with given sum/2-sum-count-distinct-pairs-with-given-sum.java
+++ Difficulty: Easy/2 Sum – Count distinct pairs with given sum/2-sum-count-distinct-pairs-with-given-sum.java
@@ -1,27 +1,30 @@
 class Solution {
 
     static int countDistinctPairs(int arr[], int target) {
-        
         // Your code here
+        if(arr.length<1){
+            return 0;
+        }
         Arrays.sort(arr);
-        int left =0;int right=arr.length-1;
+        int left=0;
+        int right=arr.length-1;
         int count=0;
         while(left<right){
             if(left>0 && arr[left-1]==arr[left]){
                 left++;
                 continue;
             }
-            int sum=arr[left]+arr[right];
-            if(sum==target){
+            if(arr[left]+arr[right]==target){
                 count++;
                 left++;
                 right--;
-            }else if(sum<target){
+            }else if(arr[left]+arr[right]<target){
                 left++;
             }else{
                 right--;
             }
         }
+        
         return count;
     }
 }
