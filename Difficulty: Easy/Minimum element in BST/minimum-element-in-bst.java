f2da82b00906d857c35feea782d8ed4356e55973
Nayan Wasnik <139579453+nayanwasnik30@users.noreply.github.com>
2025-11-13 09:26:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum element in BST/minimum-element-in-bst.java
@@ -0,0 +1,21 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Solution {
+    public int minValue(Node root) {
+        // code here
+        if(root.left==null){
+            return root.data;
+        }
+        else return minValue(root.left);
+    }
+}
